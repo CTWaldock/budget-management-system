3a376143ef2d0121be1fd33c2ae3d733c3d7860c
viparthasarathy <viparthasarathy@gmail.com>
2016-05-26 14:45:33 -0700

    update user model specs to test for oauth creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,15 @@ describe User do
     user = FactoryGirl.create(:user, email: 'hey@yahoo.com')
     expect(user.email).to eq('hey@yahoo.com')
   end
+
+  it 'can create users through .from_omniauth' do
+    auth = {
+      provider: 'fake',
+      uid: 100000,
+      info: {email: 'fake@yahoo.com'}
+    }
+    user = User.from_omniauth(auth)
+    expect(user).to eq(User.last)
+  end
+
 end
