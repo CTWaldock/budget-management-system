1ff495b377f4a9567fe6f8114795f55bde7075b3
viparthasarathy <viparthasarathy@gmail.com>
2016-05-27 19:50:34 -0700

    add budget feature test for redirect if no timezone

--- spec/features/budget/budget_creation_spec.rb
+++ spec/features/budget/budget_creation_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 describe 'budget creation' do
 
+  before do
+    @user = FactoryGirl.create(:user, time_zone: nil)
+  end
+
   it 'requires users to have a timezone before allowing them to access the budget creation page' do
+    login_as(user, :scope => :user)
     visit new_budget_path
-    expect(page).to have_content("Please set a timezone.")
+    expect(page).to have_content("Set your timezone!")
   end
 
 end
