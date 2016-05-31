426e8ef3f9d661466f96677b946ea8bd13130688
viparthasarathy <viparthasarathy@gmail.com>
2016-05-30 17:57:07 -0700

    add budget index page

--- spec/features/budgets/budget_edit_spec.rb
+++ spec/features/budgets/budget_edit_spec.rb
@@ -2,7 +2,22 @@ require 'rails_helper'
 
   describe 'budget edit' do
 
-    it 'allows users to add new categories' do
+    before do
+      @user = FactoryGirl.create(:user)
+      @budget = FactoryGirl.create(:budget)
     end
 
+    it 'allows users to add new categories' do
+      login_as(@user, :scope => :user)
+    end
+
+    it 'allows users to edit category titles' do
+    end
+
+    it 'allows users to edit budget details' do
+    end
+
+    it 'allows users to delete categories' do
+    end
+    
   end
