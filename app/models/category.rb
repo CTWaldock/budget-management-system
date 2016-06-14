efdf675cdf6a533810fa3ea5f5be1183b5757332
viparthasarathy <viparthasarathy@gmail.com>
2016-06-14 15:03:56 -0700

    refactor expense callbacks, add category destroy callback

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,17 +1,28 @@
 class Category < ActiveRecord::Base
   belongs_to :budget
   has_many :expenses
+  before_destroy :delete_expenses_and_subtract_from_budget
+  # could potentially use :dependent => destroy_all instead which would trigger the before destroy expense callback to subtract cost from budget, but would be more time intensive
 
   def current_percentage
-    if self.budget.total_expense == 0
+    if budget.total_expense == 0
       return 0
     else
-      ((self.subtotal / self.budget.total_expense) * 100).round
+      ((self.subtotal / budget.total_expense) * 100).round
     end
   end
 
   def total_percentage
-    ((self.subtotal / self.budget.limit) * 100).round
+    ((self.subtotal / budget.limit) * 100).round
+  end
+
+  private
+
+  def delete_expenses_and_subtract_from_budget
+    budget.total_expense -= self.subtotal
+    budget.save
+
+    self.expenses.delete_all
   end
 
 end
--- spec/features/categories/category_page_spec.rb
+++ spec/features/categories/category_page_spec.rb
@@ -2,18 +2,52 @@ require 'rails_helper'
 
 describe 'category page' do
 
-  it 'lists information regarding the category' do
-  end
 
-  it 'allows the user to return to the budget' do
+  before do
+    @user = FactoryGirl.create(:user)
+    @budget = FactoryGirl.create(:budget, user: @user, limit: 100)
+    @category = @budget.categories.first
+    @other_category = FactoryGirl.create(:category, title: "Gas", budget: @budget)
+    FactoryGirl.create(:expense, category: @category)
+    FactoryGirl.create(:expense, description: "Burger", cost: 9.50, category: @category)
   end
 
   it 'restricts access to owner of budget only' do
+    other_user = FactoryGirl.create(:user, email: "jim@yahoo.com")
+    login_as(other_user, :scope => :user)
+
+    visit category_page(@category)
+    expect(page).to have_content("You don't have access to that budget.")
+    expect(page).to_not have_content(@category.title)
+  end
+
+
+  it 'lists information regarding the category' do
+    login_as(@user, :scope => :user)
+    visit category_page(@category)
+    expect(page).to have_content("Food")
+    expect(page).to have_content("$20.00")
+    expect(page).to have_content("100%")
+    expect(page).to have_content("20%")
+  end
+
+  it 'allows the user to return to the budget' do
+    login_as(@user, :scope => :user)
+    visit category_page(@category)
+    click_link "Return to Budget"
+    expect(page.current_path).to eq budget_path(@budget)
   end
 
   it 'allows users to delete categories and adjusts budget accordingly' do
+    login_as(@user, :scope => :user)
+    visit category_page(@category)
+    click_link "Delete Category"
+    expect(page.current_path).to eq budget_path(@budget)
+    expect(page).to_not have_content("Food")
+    expect(page).to have_content("Current Expenditure: $0.00")
+    expect(@budget.total_expense).to eq(0)
+    expect(@budget.categories.count).to eq(1)
   end
-
 
   it 'allows users to add new expenses and adjusts subtotal and budget accordingly' do
   end
