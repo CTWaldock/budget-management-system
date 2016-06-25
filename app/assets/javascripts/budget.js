8a781d61a9d3df8e19eb7a5634822487f58502f3
viparthasarathy <viparthasarathy@gmail.com>
2016-06-25 15:25:04 -0700

    add method to budget prototype
    exceeded function that returns true or false dependent on whether budget is exceeded

--- app/assets/javascripts/budget.js
+++ app/assets/javascripts/budget.js
@@ -4,3 +4,7 @@ function Budget (id, limit, totalExpense, category) {
   this.totalExpense = totalExpense;
   this.category = category;
 }
+
+Budget.prototype.exceeded = function() {
+  return (this.totalExpense > this.limit);
+}
