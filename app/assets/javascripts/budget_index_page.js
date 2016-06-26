417af41e180723d9207a97da2f2e9780e20d924d
viparthasarathy <viparthasarathy@gmail.com>
2016-06-26 11:04:32 -0700

    minor refactoring

--- app/assets/javascripts/budget_index_page.js
+++ app/assets/javascripts/budget_index_page.js
@@ -1,13 +1,3 @@
-function insertBudgets(budgetList, selector) {
-  $.each(budgetList, function(index, params) {
-    var budget = new Budget(params.name, params.id, params.limit, params.total_expense);
-    $(selector).append('<a href="/budgets/' + budget.id + '"><p>' + budget.name + '</p></a>');
-    if (budget.exceeded()) {
-      $(selector).children().last().addClass("warning");
-    }
-  });
-}
-
 function populateIndex() {
   $.get('/user/budgets', function(data) {
     $("#active").append('<h3>Active Budgets</h3>');
@@ -17,4 +7,14 @@ function populateIndex() {
     $("#completed").append('<h3>Completed Budgets</h3>');
     insertBudgets(data.completed, "#completed");
   }, "JSON")
+}
+
+function insertBudgets(budgetList, selector) {
+  $.each(budgetList, function(index, params) {
+    var budget = new Budget(params.name, params.id, params.limit, params.total_expense);
+    $(selector).append('<a href="/budgets/' + budget.id + '" data-remote="true"><p>' + budget.name + '</p></a>');
+    if (budget.exceeded()) {
+      $(selector).children().last().addClass("warning");
+    }
+  });
 }
