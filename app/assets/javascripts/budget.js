e59a1df227963ad90da6e11c081f1e44b00b8ac9
viparthasarathy <viparthasarathy@gmail.com>
2016-06-28 09:18:06 -0700

    remove budget_index_page.js, move functions to budget.js

--- app/assets/javascripts/budget.js
+++ app/assets/javascripts/budget.js
@@ -9,3 +9,24 @@ function Budget (name, id, limit, totalExpense, category) {
 Budget.prototype.exceeded = function() {
   return (this.totalExpense > this.limit);
 }
+
+function populateIndex() {
+  $.get('/user/budgets', function(data) {
+    $("#active").append('<h3>Active Budgets</h3>');
+    insertBudgets(data.active, "#active");
+    $("#inactive").append('<h3>Inactive Budgets</h3>');
+    insertBudgets(data.inactive, "#inactive");
+    $("#completed").append('<h3>Completed Budgets</h3>');
+    insertBudgets(data.completed, "#completed");
+  }, "JSON")
+}
+
+function insertBudgets(budgetList, selector) {
+  $.each(budgetList, function(index, params) {
+    var budget = new Budget(params.name, params.id, params.limit, params.total_expense);
+    $(selector).append('<a href="/budgets/' + budget.id + '"><p>' + budget.name + '</p></a>');
+    if (budget.exceeded()) {
+      $(selector).children().last().addClass("warning");
+    }
+  });
+}
