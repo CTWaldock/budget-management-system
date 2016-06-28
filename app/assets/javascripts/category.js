dfcfe248ac69797adc794e38145810965b25be98
viparthasarathy <viparthasarathy@gmail.com>
2016-06-28 09:11:38 -0700

    remove category_page.js, move functions to category.js

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -17,9 +17,27 @@ Category.prototype.currentPercent = function() {
   } else {
     return 0;
   }
-
 }
 
 Category.prototype.totalPercent = function() {
   return Math.round(this.subtotal * 100 / this.budget.limit);
+}
+
+
+
+function updateCategory(category) {
+  $('#cost').text("$" + category.subtotal.toFixed(2));
+  $('#current').text(category.currentPercent() + "%");
+  $('#total').text(category.totalPercent() + "%");
+}
+
+function fillStaticCategoryInfo(category) {
+  $('.category.name').text(category.title);
+  $('.budget.name').text(category.budget.name);
+}
+
+function fillCategoryLinks(category) {
+  $('#new_expense').attr("action", "/categories/" + category.id + "/expenses");
+  $('#budgetlink').append('<a href="/budgets/' + category.budget.id + '">Return to Budget</a>');
+  $('#deletecategorylink').append('<a href="/categories/' + category.id + '" data-method="delete">Delete this Category</a>');
 }
