d323d37a43ca2eb1284adc919b968d1aa5f37f1f
viparthasarathy <viparthasarathy@gmail.com>
2016-06-28 08:50:46 -0700

    move functions related to expense to expense.js

--- app/assets/javascripts/expense.js
+++ app/assets/javascripts/expense.js
@@ -7,3 +7,13 @@ function Expense (id, cost, description) {
 Expense.prototype.percentage = function() {
   return Math.round(this.cost * 100 / this.category.subtotal);
 }
+
+function resetErrors() {
+  $('#error_message').text("");
+  $('#error_list').empty();
+}
+
+function resetExpense() {
+  $('tbody').empty();
+  $('form')[0].reset();
+}
