965024400e08b5849d9d77b1a40bc57553a7990d
viparthasarathy <viparthasarathy@gmail.com>
2016-06-30 14:47:04 -0700

    Add test regarding dependent relationship
    Test that expense is deleted after category is destroyed

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -17,9 +17,9 @@ describe Category do
 
       it 'subtracts its subtotal from budget when deleted' do
         FactoryGirl.create(:expense, category: @category)
-        expect(@budget.current_expense).to eq(10.50)
+        expect(@budget.total_expense).to eq(10.50)
         @category.destroy
-        expect(@budget.current_expense).to eq(0)
+        expect(@budget.total_expense).to eq(0)
       end
 
     end
@@ -31,6 +31,9 @@ describe Category do
       end
 
       it 'destroys its expenses upon deletion' do
+        expense = FactoryGirl.create(:expense, category: @category)
+        @category.destroy
+        expect(Expense.all).to_not include(expense)
       end
 
     end
