faa181fa32dab01fa098cc50c51f0f5f63438f31
viparthasarathy <viparthasarathy@gmail.com>
2016-06-30 14:31:19 -0700

    add budget model specs regarding expenses
    test for most recent expenses and most expensive expenses

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -8,12 +8,47 @@ describe Budget do
 
   context 'associations' do
 
-    it 'has many categories' do
-      expect(@budget).to respond_to(:categories)
+    describe 'categories' do
+
+      before do
+        FactoryGirl.create(:expense, category: @budget.categories.first)
+        category = FactoryGirl.create(:category, title: "Diamonds", budget: @budget)
+        FactoryGirl.create(:expense, cost: 4000, description: "diamond", category: category)
+      end
+
+      it 'has many categories' do
+        expect(@budget).to respond_to(:categories)
+      end
+
+      it 'knows how to order categories based on subtotal' do
+        expect(@budget.categories.first.title).to_not eq("Diamonds")
+        expect(@budget.ordered_categories.first.title).to eq("Diamonds")
+      end
+
     end
 
-    it 'has many individual expenses through categories' do
-      expect(@budget).to respond_to(:expenses)
+    describe 'expenses' do
+
+      before do
+        FactoryGirl.create(:expense, category: @budget.categories.first)
+        FactoryGirl.create(:expense, cost: 20, description: "Burger", category: @budget.categories.first)
+        FactoryGirl.create(:expense, cost: 5, description: "Coke", category: @budget.categories.first)
+      end
+
+      it 'knows how to order expenses based on recency' do
+        expect(@budget.most_recent_expenses.first.description).to eq("Coke")
+        expect(@budget.most_recent_expenses.last.description).to eq("Pizza")
+      end
+
+      it 'knows how to order expenses based on price' do
+        expect(@budget.top_five_expenses.first.description).to eq("Burger")
+        expect(@budget.top_five_expenses.last.description).to eq("Coke")
+      end
+
+      it 'has many individual expenses through categories' do
+        expect(@budget).to respond_to(:expenses)
+      end
+
     end
 
   end
