ac0c050bfa49141dad9bef65ba82dda46992a750
viparthasarathy <viparthasarathy@gmail.com>
2016-06-30 11:43:59 -0700

    add explicit status response

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -14,9 +14,9 @@ class ExpensesController < ApplicationController
   def destroy
     @expense = Expense.find(params[:id])
     @category = @expense.category
-    
+    authorize @expense
     @expense.destroy
-    render json: @category
+    render json: @category, status: 200
   end
 
   private
