ae2f0c1c7d3cd155157300584d989796582978db
viparthasarathy <viparthasarathy@gmail.com>
2016-07-16 17:14:38 -0700

    Expand on note in expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -12,7 +12,7 @@ class Expense < ActiveRecord::Base
 
   private
 
-  # NOTE: Refactor to use ActiveRecord queries instead of storing redundant data in budget and category tables.
+  # NOTE: Alternatively, use ActiveRecord to sum expenses for both category and budget. Current approach breaks database normalization rules by introducing redundant information into category and budget and is only necessary in case of actual performance issues. Consider refactoring -- currently a case of premature optimization.
 
   def add_to_budget_and_category
     budget = category.budget
